a862c4cbdd6e5898cb89a9c637b46c00c46f4164
hms2002 <105370956+20211101@users.noreply.github.com>
2026-03-23 23:48:04 +0900

    3주차 프로젝트

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(h, v, 0) * speed * Time.deltaTime;
+    }
+}
