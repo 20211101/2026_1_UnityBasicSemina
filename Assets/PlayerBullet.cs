620c64eef44fd0c4e6b35646c62a57ea090277ff
hms2002 <105370956+20211101@users.noreply.github.com>
2026-05-08 15:44:39 +0900

    rigidbody 이용한 코드 빼기(아직 이름)

--- Assets/PlayerBullet.cs
+++ Assets/PlayerBullet.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class PlayerBullet : MonoBehaviour
 {
-    Rigidbody2D rb;
     float speed = 5f;
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = transform.up * speed;
+        transform.position += transform.up * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
